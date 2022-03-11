3e06c74dd452b6fbe92a73c56421502363a4194d
GabrielaMitu <gabrielaym@al.insper.edu.br>
2022-03-11 14:57:26 -0300

    limpeza

--- main.py
+++ main.py
@@ -7,81 +7,6 @@ from models import User, Gender, Role, Product
 
 app = FastAPI()
 
-# db: List[User] = [
-#     User(
-#         id = UUID("0422058c-efa8-4a1d-8fae-4229fb6abfe5"),
-#         first_name = "Jamila",
-#         last_name = "Ahmed",
-#         gender = Gender.female,
-#         roles = [Role.student]
-#     ),
-#     User(
-#         id = UUID("a8db539d-2303-4cda-b174-c7edd84435fd"),
-#         first_name = "Alex",
-#         last_name = "Jones",
-#         gender = Gender.male,
-#         roles = [Role.admin, Role.user]
-#     )
-# ]
-
-# db2:   List[Product] = [
-#     Product(
-#         id = UUID("SE93QE"),
-#         price = 10.2,
-#         name = "Pao de queijo"
-#     ),
-#     Product(
-#         id = UUID("fgvdthgbrt"),
-#         price = 20.0,
-#         name = "Cafe de vinte conto"
-#     ),
-#     Product(
-#         id = UUID("vvbdgfbdteg"),
-#         price = 15,
-#         name = "Iogurte"
-#     ), 
-
-#     ]
-    
-
-
-#     List[Product] = [
-#         Product(
-#             id = UUID("SE93QE"),
-#             price = 10.2,
-#             name = "Pao de queijo"
-#         ),
-#         Product(
-#             id = UUID("fgvdthgbrt"),
-#             price = 20.0,
-#             name = "Cafe de vinte conto"
-#         ),
-#         Product(
-#             id = UUID("vvbdgfbdteg"),
-#             price = 15,
-#             name = "Iogurte"
-#         ), 
-
-#     ]
-
-# db2:    List[Product] = [
-#         Product(
-#             id = UUID("SE93QE"),
-#             price = 10.2,
-#             name = "Pao de queijo"
-#         ),
-#         Product(
-#             id = UUID("fgvdthgbrt"),
-#             price = 20.0,
-#             name = "Cafe de vinte conto"
-#         ),
-#         Product(
-#             id = UUID("vvbdgfbdteg"),
-#             price = 15,
-#             name = "Iogurte"
-#         ), 
-
-#     ]
 
 db = {
     'user': [
@@ -120,42 +45,7 @@ db = {
         ),
     ]
 }
-#     User(
-#         id = UUID("0422058c-efa8-4a1d-8fae-4229fb6abfe5"),
-#         first_name = "Jamila",
-#         last_name = "Ahmed",
-#         gender = Gender.female,
-#         roles = [Role.student]
-#     ),
-#     User(
-#         id = UUID("a8db539d-2303-4cda-b174-c7edd84435fd"),
-#         first_name = "Alex",
-#         last_name = "Jones",
-#         gender = Gender.male,
-#         roles = [Role.admin, Role.user]
-#     ),
-#     Product(
-#         id = UUID("SE93QE"),
-#         price = 10.2,
-#         name = "Pao de queijo"
-#     ),
-#     Product(
-#         id = UUID("fgvdthgbrt"),
-#         price = 20.0,
-#         name = "Cafe de vinte conto"
-#     ),
-#     Product(
-#         id = UUID("vvbdgfbdteg"),
-#         price = 15,
-#         name = "Iogurte"
-#     ), 
 
-# ]
-   
-
-@app.get("/")
-async def root():
-    return {"Hello": "World"}
 
 @app.get("/carrinho/{carrinho_id}/")
 def read_items(carrinho_id: str):
