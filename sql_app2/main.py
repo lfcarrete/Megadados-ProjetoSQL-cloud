bfcf751d70f46332fcaf3cf2c1b4b69e48c83aa7
GabrielaMitu <gabrielaym@al.insper.edu.br>
2022-05-13 15:23:49 -0300

    get de um unico item

--- sql_app2/main.py
+++ sql_app2/main.py
@@ -53,3 +53,10 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
