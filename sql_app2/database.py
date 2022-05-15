50054742101e1f55dd1c302a6ebb1f950a936c54
TheoBR00 <38351029+TheoBR00@users.noreply.github.com>
2022-05-15 16:14:21 -0300

    Teste env 4

--- sql_app2/database.py
+++ sql_app2/database.py
@@ -11,6 +11,8 @@ PASSWORD = os.getenv('USERNAME')
 HOST = os.getenv('USERNAME')
 DATABASE = os.getenv('USERNAME')
 
+#
+
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app2.db"
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + "/" + DATABASE
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
