a12de976afcaf634bfe797a2a84787e8f3ac9c8e
Luis Filipe Carrete <lfcsanchez@live.com>
2022-05-18 08:40:42 -0300

    Update database.py

--- sql_app2/database.py
+++ sql_app2/database.py
@@ -9,22 +9,22 @@ import os
 from dotenv import load_dotenv
 load_dotenv('DB.env')
 
-USERNAME = os.environ.get("DB_USERNAME")
-PASSWORD = os.environ.get("DB_PASSWORD")
-HOST = os.environ.get("DB_HOST")
-DATABASE = os.environ.get("DB_DATABASE")
+#USERNAME = os.environ.get("DB_USERNAME")
+#PASSWORD = os.environ.get("DB_PASSWORD")
+#HOST = os.environ.get("DB_HOST")
+#DATABASE = os.environ.get("DB_DATABASE")
 
 ##########
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app2.db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + "/" + DATABASE
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app2.db"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + "/" + DATABASE
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-if not database_exists(engine.url):
-    create_database(engine.url)
+#if not database_exists(engine.url):
+#    create_database(engine.url)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
